c7d4fcacb965d6e304717c43a8fdc0261993b63b
jed1337 <j.caychingco@Yahoo.com>
2016-03-11 12:59:54 +0800

    Initial Commit

---
+++ SocketWebBrowser/src/socketwebbrowser/SocketWebBrowser.java
@@ -0,0 +1,51 @@
+package socketwebbrowser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SocketWebBrowser {
+   
+   public SocketWebBrowser(){
+      try {
+//         String host = "www.dlsu.edu.ph";
+         String host = "localhost";
+         Socket socket = new Socket(host, 80);
+         BufferedReader in = new BufferedReader(new InputStreamReader(
+                 socket.getInputStream()));
+         PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+         
+         System.out.println("Entering while");
+         String message =
+            "GET / HTTP/1.1\n"+
+            "Host: "+ host+"\n"+
+            "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0\n"+
+            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n"+
+            "Accept-Language: en-US,en;q=0.5\n"+
+            "Accept-Encoding: gzip, deflate\n"+
+            "Connection: keep-alive\n\n";
+         System.out.println(message);
+         output.println(message);
+         
+         System.out.println("Send output done");
+         while (true) {
+            String line = in.readLine();
+            if(line!=null){
+               System.out.println(line);
+            }
+         }
+      } catch (IOException ex) {
+         Logger.getLogger(SocketWebBrowser.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      
+   }
+
+   public static void main(String[] args) {
+      SocketWebBrowser sg = new SocketWebBrowser();
+//      sg.
+   }
+}
---
+++ SocketWebServer/src/socketwebserver/SocketWebServer.java
@@ -0,0 +1,48 @@
+package socketwebserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SocketWebServer {
+   public static void main(String[] args) {
+      
+      try {
+         ServerSocket ss = new ServerSocket(80);
+         System.out.println("Server Made");
+
+         while (true) {
+            System.out.println("\nStart while loop");
+            Socket s = ss.accept();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(s.getInputStream()));
+
+            PrintWriter output = new PrintWriter(s.getOutputStream(), true);
+
+            String line;
+            while (!(line = br.readLine()).equals("")) {
+               System.out.println(line);
+            }
+
+            System.out.println("End read buffered reader");
+
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html");
+            output.println("Server: Bot");
+            output.println("");
+            output.println("<H1>Hi World</H1>");
+
+            System.out.println("Done printing output");
+            output.flush();
+            s.close();
+         }
+      } catch (IOException ex) {
+         Logger.getLogger(SocketWebServer.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+}
