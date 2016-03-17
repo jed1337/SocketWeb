ad2b84135e1667b05ae4bb6e7e155070f02a31ee
jed1337 <j.caychingco@Yahoo.com>
2016-03-17 12:13:36 +0800

    Organized server code

--- SocketWebServer/src/socketwebserver/SocketWebServer.java
+++ SocketWebServer/src/socketwebserver/SocketWebServer.java
@@ -9,69 +9,66 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
-import static java.lang.System.in;
 import java.util.Arrays;
+import static java.lang.System.in;
 
 public class SocketWebServer {
    public static void main(String[] args) throws IOException {
       System.out.println("The chat server is running.");
-      ServerSocket ss = new ServerSocket(8080);
-      try {
+      
+//      String fileName = "src/Assets/HighMufasa.png";
+      String fileName = "src\\Assets\\HighMufasa.png";
+
+      try (ServerSocket ss = new ServerSocket(8080)) {
          while (true) {
-            new Handler(ss.accept()).start();
+            new Handler(ss.accept(), fileName).start();
          }
-      } finally {
-         ss.close();
       }
    }
 
    private static class Handler extends Thread {
-      private Socket client;
+      private final Socket client;
+      private final String fileName;
 
-      public Handler(Socket socket) {
+      public Handler(Socket socket, String fileName) {
          this.client = socket;
+         this.fileName = fileName;
+
+         System.out.println("Client accepted");
       }
 
       @Override
       public void run() {
          try {
-            System.out.println("SocketWebServer Made");
-            System.out.println("\nStart while loop");
-
-            System.out.println("End read buffered reader");
-
             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-            out.println("HTTP/1.1 200 OK");
-            out.println("Content-Type: image/png");
-            out.println("SocketWebServer: Bot");
-            out.println("");
-
-//                    String fileName = "src/Assets/HighMufasa.png";
-            String fileName = "src\\Assets\\HighMufasa.png";
-
-            File file = new File(fileName);
-
-            ImageInputStream imageInputStream = ImageIO.createImageInputStream(file);
-            long size = imageInputStream.length();
-            out.write("Content-Length: " + size + "\r\n");
-            out.write("\r\n");
+            File file       = new File(fileName);
+            printHeaders(out, file);
 
             BufferedImage bufferedImage = ImageIO.read(file);
-            boolean success = ImageIO.write(bufferedImage, "png", client.getOutputStream());
+            boolean success             = ImageIO.write(bufferedImage, "png", client.getOutputStream());
             System.out.println("Success: " + success); //true
             System.out.println("Done printing output");
 
             out.flush();
 
             close(out);
-            out.close();
             close(in);
             close(client);
-//                }
          } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-            System.out.println("ERROR");
+            printError(ex);
          }
+      }
+
+      private void printHeaders(PrintWriter out, File file) throws IOException {
+         out.println("HTTP/1.1 200 OK");
+         out.println("Content-Type: image/png");
+         out.println("SocketWebServer: Bot");
+         out.println("");
+
+         ImageInputStream imageInputStream = ImageIO.createImageInputStream(file);
+         long size = imageInputStream.length();
+         out.write("Content-Length: " + size + "\r\n");
+         out.write("\r\n");
       }
    }
 
@@ -82,9 +79,12 @@ public class SocketWebServer {
       try {
          c.close();
       } catch (IOException ex) {
-         System.err.println("ERROR");
-         System.err.println(ex.getMessage());
-         System.err.println(Arrays.toString(ex.getStackTrace()));
+         printError(ex);
       }
+   }
+
+   private static void printError(IOException ex) {
+      System.err.println("ERROR" :+ex.getMessage());
+      System.err.println(Arrays.toString(ex.getStackTrace()));
    }
 }
