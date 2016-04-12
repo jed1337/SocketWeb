68dbb6e12250c66978107ec5e5830c8a9291fbdf
jed1337 <j.caychingco@yahoo.com>
2016-04-12 21:23:15 +0800

    Using internet example

--- SocketWebBrowser/src/socketwebbrowser/SocketWebBrowser.java
+++ SocketWebBrowser/src/socketwebbrowser/SocketWebBrowser.java
@@ -1,60 +1,126 @@
 package socketwebbrowser;
+//
+//import java.io.BufferedInputStream;
+//import java.io.IOException;
+//import java.io.OutputStream;
+//import java.net.Socket;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//
+//public class SocketWebBrowser extends Thread {
+//   private final int number;
+//   private final Socket socket;
+//   private static final int THREAD_COUNT = 8;
+//
+//   public SocketWebBrowser(int number) throws IOException {
+//      System.out.println("Number: " + number);
+//      this.number = number;
+//
+//      socket = new Socket("localhost", 8080);
+//      System.out.println(socket);
+//   }
+//
+//   @Override
+//   public void run() {
+//      try {
+//         BufferedInputStream b = new BufferedInputStream(socket.getInputStream());
+//         OutputStream out      = socket.getOutputStream();
+//         
+//         int count;
+//         byte buffer[] = new byte[1024];
+//         
+//         int i = 0;
+//
+//         while ((count = b.read(buffer)) != -1) {
+//            out.write(buffer, 0, count);
+//            System.out.println("i = " + (i++));
+//         }
+//
+//         System.out.println("File received, number " + number);
+//      } catch (IOException ex) {
+//         System.err.println("ERROR File: " + ex + " Number: " + number);
+//      } finally {
+//         try {
+//            socket.close();
+//         } catch (IOException ex) {
+//            System.err.println("Error closing socket :" + number);
+//            Logger.getLogger(SocketWebBrowser.class.getName()).log(Level.SEVERE, null, ex);
+//         }
+//      }
+//   }
+//
+//   public static void main(String ar[]) throws IOException {
+//      int i = 0;
+////        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
+////        while(true){
+////            executor.submit(new SocketWebBrowser(++i));
+////        }
+//      new Thread(new SocketWebBrowser(++i)).start();
+//   }
+//}
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class SocketWebBrowser extends Thread {
-   private final int number;
-   private final Socket socket;
-   private static final int THREAD_COUNT = 8;
+import java.io.*;
+import java.net.*;
 
-   public SocketWebBrowser(int number) throws IOException {
-      System.out.println("Number: " + number);
-      this.number = number;
-
-      socket = new Socket("localhost", 8080);
-      System.out.println(socket);
-   }
-
-   @Override
-   public void run() {
+public class SocketWebBrowser {
+   public static void main(String[] args) {
+// declaration section:
+// SocketWebBrowser: our client socket
+// os: output stream
+// is: input stream
+      Socket smtpSocket = null;
+      DataOutputStream os = null;
+      DataInputStream is = null;
+// Initialization section:
+// Try to open a socket on port 25
+// Try to open input and output streams
       try {
-         BufferedInputStream b = new BufferedInputStream(socket.getInputStream());
-         OutputStream out      = socket.getOutputStream();
-         
-         int count;
-         byte buffer[] = new byte[1024];
-         
-         int i = 0;
-
-         while ((count = b.read(buffer)) != -1) {
-            out.write(buffer, 0, count);
-            System.out.println("i = " + (i++));
-         }
-
-         System.out.println("File received, number " + number);
-      } catch (IOException ex) {
-         System.err.println("ERROR File: " + ex + " Number: " + number);
-      } finally {
+         smtpSocket = new Socket("localhost", 2000);
+         System.out.println("Client received");
+         os = new DataOutputStream(smtpSocket.getOutputStream());
+         is = new DataInputStream(smtpSocket.getInputStream());
+      } catch (UnknownHostException e) {
+         System.err.println("Don't know about host: hostname");
+      } catch (IOException e) {
+         System.err.println("Couldn't get I/O for the connection to: hostname");
+      }
+// If everything has been initialized then we want to write some data
+// to the socket we have opened a connection to on port 25
+      if (smtpSocket != null && os != null && is != null) {
          try {
-            socket.close();
-         } catch (IOException ex) {
-            System.err.println("Error closing socket :" + number);
-            Logger.getLogger(SocketWebBrowser.class.getName()).log(Level.SEVERE, null, ex);
+// The capital string before each colon has a special meaning to SMTP
+// you may want to read the SMTP specification, RFC1822/3
+            os.writeBytes("HELO\n");
+            os.writeBytes("MAIL From: k3is@fundy.csd.unbsj.ca\n");
+            os.writeBytes("RCPT To: k3is@fundy.csd.unbsj.ca\n");
+            os.writeBytes("DATA\n");
+            os.writeBytes("From: k3is@fundy.csd.unbsj.ca\n");
+            os.writeBytes("Subject: testing\n");
+            os.writeBytes("Hi there\n"); // message body
+            os.writeBytes("\n.\n");
+            os.writeBytes("QUIT");
+// keep on reading from/to the socket till we receive the "Ok" from SMTP,
+// once we received that then we want to break.
+            String responseLine;
+            while ((responseLine = is.readLine()) != null) {
+               System.out.println("Server: " + responseLine);
+               if (responseLine.indexOf("Ok") != -1) {
+                  break;
+               }
+            }
+// clean up:
+// close the output stream
+// close the input stream
+// close the socket
+            os.close();
+            is.close();
+            smtpSocket.close();
+         } catch (UnknownHostException e) {
+            System.err.println("Trying to connect to unknown host: " + e);
+         } catch (IOException e) {
+            System.err.println("IOException:  " + e);
          }
       }
-   }
-
-   public static void main(String ar[]) throws IOException {
-      int i = 0;
-//        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
-//        while(true){
-//            executor.submit(new SocketWebBrowser(++i));
-//        }
-      new Thread(new SocketWebBrowser(++i)).start();
    }
 }
--- SocketWebServer/src/socketwebserver/SocketWebServer.java
+++ SocketWebServer/src/socketwebserver/SocketWebServer.java
@@ -1,90 +1,132 @@
 package socketwebserver;
+//
+//import java.io.Closeable;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.io.OutputStream;
+//import java.net.ServerSocket;
+//import java.net.Socket;
+//import java.util.Arrays;
+//import static java.lang.System.in;
+//
+//public class SocketWebServer {
+//    private static final String FILE_NAME = "src/Assets/05_mb_file.txt";
+//    private static final int THREAD_COUNT = 8;
+//    
+//   public static void main(String[] args) throws IOException {
+//      System.out.println("The chat server is running.");
+//      
+//
+//      try (ServerSocket ss = new ServerSocket(8080)) {
+//         new Thread(new Handler(ss.accept(), FILE_NAME)).start();
+//         
+////        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
+////         while (true) {
+////            executor.submit(new Handler(ss.accept(), FILE_NAME));
+////         }
+//      }
+//   }
+//
+//   private static class Handler extends Thread {
+//      private final Socket client;
+//      private final String fileName;
+//
+//      public Handler(Socket socket, String fileName) {
+//         this.client   = socket;
+//         this.fileName = fileName;
+//
+//         System.out.println("Client accepted");
+//      }
+//
+//      @Override
+//      public void run() {
+//         try {
+//            
+//            OutputStream out     = client.getOutputStream();
+//            FileInputStream file = new FileInputStream(fileName);
+//            printHeaders(out);
+//            
+//            byte buffer[] = new byte[1024];
+//            int LENGTH    = buffer.length;
+//            int count;
+//            
+//            while ((count = file.read(buffer)) != -1){
+//                out.write(buffer);
+//            }
+//            
+//            out.flush();
+//
+//            safeClose(out);
+//            safeClose(in);
+//            safeClose(client);
+//         } catch (IOException ex) {
+//            printError(ex);
+//         }
+//      }
+//
+//      private void printHeaders(OutputStream out) throws IOException {
+//         out.write("HTTP/1.1 200 OK\r\n".getBytes());
+//         out.write("Content-Type: text/html\r\n".getBytes());
+//         out.write("SocketWebServer: Bot\r\n".getBytes());
+//         out.write("\r\n".getBytes());
+//      }
+//   }
+//
+//   private static void safeClose(Closeable c) {
+//      if (c == null) {
+//         return;
+//      }
+//      try {
+//         c.close();
+//      } catch (IOException ex) {
+//         printError(ex);
+//      }
+//   }
+//
+//   private static void printError(IOException ex) {
+//      System.err.println("ERROR" + ex.getMessage());
+//      System.err.println(Arrays.toString(ex.getStackTrace()));
+//   }
+//}
+//
 
-import java.io.Closeable;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Arrays;
-import static java.lang.System.in;
+import java.io.*;
+import java.net.*;
 
 public class SocketWebServer {
-    private static final String FILE_NAME = "src/Assets/05_mb_file.txt";
-    private static final int THREAD_COUNT = 8;
-    
-   public static void main(String[] args) throws IOException {
-      System.out.println("The chat server is running.");
-      
-
-      try (ServerSocket ss = new ServerSocket(8080)) {
-         new Thread(new Handler(ss.accept(), FILE_NAME)).start();
-         
-//        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
-//         while (true) {
-//            executor.submit(new Handler(ss.accept(), FILE_NAME));
-//         }
-      }
-   }
-
-   private static class Handler extends Thread {
-      private final Socket client;
-      private final String fileName;
-
-      public Handler(Socket socket, String fileName) {
-         this.client   = socket;
-         this.fileName = fileName;
-
-         System.out.println("Client accepted");
-      }
-
-      @Override
-      public void run() {
-         try {
-            
-            OutputStream out     = client.getOutputStream();
-            FileInputStream file = new FileInputStream(fileName);
-            printHeaders(out);
-            
-            byte buffer[] = new byte[1024];
-            int LENGTH    = buffer.length;
-            int count;
-            
-            while ((count = file.read(buffer)) != -1){
-                out.write(buffer);
-            }
-            
-            out.flush();
-
-            safeClose(out);
-            safeClose(in);
-            safeClose(client);
-         } catch (IOException ex) {
-            printError(ex);
-         }
-      }
-
-      private void printHeaders(OutputStream out) throws IOException {
-         out.write("HTTP/1.1 200 OK\r\n".getBytes());
-         out.write("Content-Type: text/html\r\n".getBytes());
-         out.write("SocketWebServer: Bot\r\n".getBytes());
-         out.write("\r\n".getBytes());
-      }
-   }
-
-   private static void safeClose(Closeable c) {
-      if (c == null) {
-         return;
-      }
+   public static void main(String args[]) {
+// declaration section:
+// declare a server socket and a client socket for the server
+// declare an input and an output stream
+      ServerSocket echoServer = null;
+      String line;
+      DataInputStream is;
+      PrintStream os;
+      Socket clientSocket = null;
+// Try to open a server socket on port 9999
+// Note that we can't choose a port less than 1023 if we are not
+// privileged users (root)
       try {
-         c.close();
-      } catch (IOException ex) {
-         printError(ex);
+         echoServer = new ServerSocket(2000);
+      } catch (IOException e) {
+         System.out.println(e);
       }
-   }
-
-   private static void printError(IOException ex) {
-      System.err.println("ERROR" + ex.getMessage());
-      System.err.println(Arrays.toString(ex.getStackTrace()));
+      System.out.println("Server up");
+// Create a socket object from the ServerSocket to listen and accept 
+// connections.
+// Open input and output streams
+      try {
+         clientSocket = echoServer.accept();
+         is = new DataInputStream(clientSocket.getInputStream());
+         os = new PrintStream(clientSocket.getOutputStream());
+// As long as we receive data, echo that data back to the client.
+         while (true) {
+            line = is.readLine();
+            System.out.println("line = " + line);
+            os.println(line);
+         }
+      } catch (IOException e) {
+         System.out.println(e);
+      }
    }
 }
